edfb13d4bc9b082c78efde7c86d7afc2bbe87723
Diego Ortiz <diegoortizmatajira@gmail.com>
2018-02-21 22:35:26 -0500

    Versión Estable con Docker

--- dockerTask.sh
+++ dockerTask.sh
@@ -1,91 +1,34 @@
 #!/bin/bash
-imageName="WebApiTemplate"
 projectName="WebApiTemplate"
-serviceName="webapi-service"
-runtimeID="debian.8-x64"
+imageName="webapi_image"
+serviceName="webapi_service"
+runtimeID="linux-x64"
 framework="netcoreapp2.0"
+prefix="local"
 
-composeClean () {
-    composeFileName=$1
-    prefix=$2
-    docker-compose -f $composeFileName -p $prefix down
-}
-
-composeBuild () {
-    composeFileName=$1
-    prefix=$2
-    echo "Building the images $prefix $imageName"
-    docker-compose -f $composeFileName -p $prefix build
-}
-
-composeUp () {
-    composeFileName=$1
-    prefix=$2
-    echo "Composing Up..."
-    if [[ $ENVIRONMENT == "debug" ]]; then
-        docker-compose -f $composeFileName -p $prefix up -d "$serviceName-debug"
+invokeDockerCompose()
+{
+    environment=$1
+    action=$2
+    if [[ $environment = "production" ]]; then
+        docker-compose -f docker-compose.yml -p $prefix $action
     else
-        docker-compose -f $composeFileName -p $prefix up -d $serviceName
+        docker-compose -f docker-compose.yml -f docker-compose.$environment.yml -p $prefix $action
     fi
 }
 
-composeKill () {
-    composeFileName=$1
-    prefix=$2
-    echo "Composing Down..."
-    if [[ $ENVIRONMENT == "debug" ]]; then
-        docker-compose -f $composeFileName -p $prefix kill "$serviceName-debug"
-    else
-        docker-compose -f $composeFileName -p $prefix kill $serviceName
-    fi
+composeForDebug()
+{
+    invokeDockerCompose debug "kill ${serviceName}"
+    invokeDockerCompose debug "up -d"
 }
 
-# Kills all running containers of an image and then removes them.
-cleanAll () {
-    if [[ $ENVIRONMENT != "release" ]]; then
-        composeClean "docker-compose.development.yml" "development"
-    else
-        composeClean "docker-compose.yml" "release"
-    fi
+cleanAll()
+{
     # Remove any dangling images (from previous builds)
     danglingImages=$(docker images -q --filter 'dangling=true')
     if [[ ! -z $danglingImages ]]; then
         docker rmi -f $danglingImages
-    fi
-}
-
-# Builds the Docker image.
-buildImage () {
-    if [[ $ENVIRONMENT != "release" ]]; then
-        echo "Building the project (Development)."
-        composeBuild "docker-compose.development.yml" "development"
-    else
-        echo "Building the project (Production)."
-        composeBuild "docker-compose.yml" "release"
-    fi
-}
-
-# Runs docker-compose.
-compose () {
-    buildImage
-    if [[ $ENVIRONMENT != "release" ]]; then
-        echo "Building the project (Development)."
-        composeKill "docker-compose.development.yml" "development"
-        composeUp "docker-compose.development.yml" "development"
-    else
-        echo "Building the project (Production)."
-        composeKill "docker-compose.yml" "release"
-        composeUp "docker-compose.yml" "release"
-    fi
-}
-
-startDebugging () {
-    containerName="development_${serviceName}-debug_1"
-    containerId=$(docker ps -f "name=$containerName" -q -n=1)
-    if [[ -z $containerId ]]; then
-        echo "Could not find a container named $containerName"
-    else
-        docker exec -i $containerId /vsdbg/vsdbg --interpreter=vscode
     fi
 }
 
@@ -97,8 +40,8 @@ showUsage () {
     echo "Commands:"
     echo "    build: Builds a Docker image ('$imageName')."
     echo "    compose: Runs docker-compose."
+    echo "    composeForDebug: Runs docker-compose ready for a debugging session."
     echo "    clean: Removes the image '$imageName' and kills all containers based on that image."
-    echo "    composeForDebug: Builds the image and runs docker-compose."
     echo "    startDebugging: Finds the running container and starts the debugger inside of it."
     echo ""
     echo "Environments:"
@@ -123,16 +66,20 @@ if [ $# -eq 0 ]; then
 else
     case "$1" in
         "compose")
-            compose
-        ;;
-        "startDebugging")
-            startDebugging
+            invokeDockerCompose $ENVIRONMENT "up -d"
         ;;
         "build")
-            buildImage
+            invokeDockerCompose $ENVIRONMENT build
         ;;
         "clean")
+            invokeDockerCompose $ENVIRONMENT down
             cleanAll
+        ;;
+        "composeForDebug")
+            composeForDebug
+        ;;
+        "startDebugging")
+            docker exec -i "${prefix}_${serviceName}_1" /vsdbg/vsdbg --interpreter=vscode
         ;;
         *)
             showUsage
